aadd6dade467a9860a83068205260a870ecc3020
Dhiground8847 <123566793+Dhiground8847@users.noreply.github.com>
2023-06-19 19:02:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var nome = "daniel";
+var sobrenome = "iwamoto";
+var nomecompleto = nome.concat (sobrenome);
+console.log(nomecompleto);
+
+var a = 6;
+var b = 10;
+
+var add = a +b;
+console.log(add);
+
+var sub = a -b;
+console.log(sub);
+
+var m = a *b;
+console.log(m);
+
+var d = a /b;
+console.log(d);
